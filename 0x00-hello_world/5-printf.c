1221e4f5f8233126055b5863e46da57297c239eb
alzheimmer <fogniebla@hotmail.com>
2019-06-19 21:14:36 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Print 
+ *
+ *Description: prints exactly with proper grammar, but the outcome
+ *is a piece of art,, followed by a new line.
+ *
+ *Return: returns 0
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
