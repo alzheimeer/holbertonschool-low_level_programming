c9ee2805578c422e47ff985ad13a5e4bc284ef28
alzheimmer <fogniebla@hotmail.com>
2019-06-19 20:47:20 +0000

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+
+/* puts() writes the string s and a trailing newline to stdout */
+
+int main() {  
+
+puts("Programming is like building a multilingual puzzle"); 
+
+ return(0);
+} 
