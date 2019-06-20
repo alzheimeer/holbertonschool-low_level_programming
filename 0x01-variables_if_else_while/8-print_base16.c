9e68950ffef6e6594faec89a5f49392905f9e447
alzheimmer <fogniebla@hotmail.com>
2019-06-20 21:25:11 +0000

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - For Practice
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char t[16] = "0123456789abcdef";
+int i;
+
+for (i = 0; i < 15; i++)
+{
+putchar(t[i]);
+}
+putchar('\n');
+return (0);
+}
