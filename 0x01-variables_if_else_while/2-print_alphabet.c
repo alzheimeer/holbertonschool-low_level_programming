27c8cd9e2af799aa5dcd439de3a27c20d84e4f8d
alzheimmer <fogniebla@hotmail.com>
2019-06-20 19:09:02 +0000

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - For Practice
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char t[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+putchar(t[i]);
+}
+putchar('\n');
+return (0);
+}
