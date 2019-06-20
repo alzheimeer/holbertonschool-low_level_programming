a9b3c76e39abf8c39038d35fc05888d3ca90042a
alzheimeer <fogniebla@hotmail.com>
2019-06-19 22:43:01 -0500

    101 sizeof and write-2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: 1
  */
 int main(void)
 {
 char f[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-int s = sizeof(f);
+int s;
+s = sizeof(f);
 write(2, f, s);
 return (1);
 }
