898acc590a90a5bb24fc52d7475149bd856f849b
alzheimmer <fogniebla@hotmail.com>
2019-06-20 19:58:22 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - For Practice
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char t[10] = "0123456789";
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(t[i]);
+}
+putchar('\n');
+return (0);
+}
