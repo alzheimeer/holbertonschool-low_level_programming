03a5012c13fb341916b7ee0f84cdcdd9a36145b1
alzheimeer <fogniebla@hotmail.com>
2019-06-25 13:21:39 -0500

    task 10 comment

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
 /**
- * add - adds two numbers together.
+ * add - adds two integers and returns the result
  * Return: i +  j
+ * @i: variable 1
+ * @j: variable 2
  */
 int add(int i, int j)
 {
