96c7167bffdb1da8c9187029314457500ae02fe2
alzheimeer <fogniebla@hotmail.com>
2019-06-25 11:08:23 -0500

    3 task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,11 @@
 #include "holberton.h"
 /**
- * main - check for lowercase
+ * _islower - check for lowercase
+ * @c: check
+ * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter
  */
-
-
-
 int _islower(int c)
 {
-
-
 	if (c >= 97 && c <= 122)
 	return (1);
 
