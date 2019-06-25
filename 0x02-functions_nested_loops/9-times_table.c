50cf59254e83cbf2b169f513508f2bf2b4a46a51
alzheimeer <fogniebla@hotmail.com>
2019-06-25 18:14:56 -0500

    check code 6

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,37 +1,37 @@
 #include "holberton.h"
+
 /**
  * times_table - prints the 9 times table, starting with 0
- * Return: 0
- *
+ * Return: 0;
  */
 
 void times_table(void)
 {
-		int i, j, k;
+	int i, j, k;
 
-		for (i = 0; i <= 9; i++)
-		{
-			for (j = 0; j <= 9; j++)
-			{
-				k = i * j;
-				if (k < 10)
-				{
-					if (j > 0)
+	for (i = 0; i <= 9; i++)
+	{
+		for(j = 0; j <= 9; j++)
+	       	{
+			k = i * j;
+		       	if (k < 10)
+	       		{
+		       		if (j > 0)
 					{
 						_putchar(',');
 						_putchar(' ');
 						_putchar(' ');
 					}
-					_putchar(k + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(k / 10 + '0');
-					_putchar(k % 10 + '0');
-				}
+				_putchar(k + '0');
 			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+		}
 			_putchar('\n');
 	}
 }
