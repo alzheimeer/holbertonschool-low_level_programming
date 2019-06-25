c3d63a32d9dac054f45bad10dbf08795d58c082d
alzheimeer <fogniebla@hotmail.com>
2019-06-25 11:21:34 -0500

    task 4 ok

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
 /**
- * main - Task 4
- *
+ * _isalpha -  checks for alphabetic character
+ * @c: check
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 
 int _isalpha(int c)
