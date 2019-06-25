f3c0901a8e207c18a10b9ea28605f8a94b0a1da4
alzheimeer <fogniebla@hotmail.com>
2019-06-25 09:58:15 -0500

    correct task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "holberton.h"
 /**
- * main - Write a function that prints the alphabet,
+ * print_alphabet - Write a function that prints the alphabet,
  * in lowercase, followed by a new line
  */
 
 void print_alphabet(void)
 {
-	char q;
-	for (q = 'a'; q <= 'z'; q++)
-	{
-	_putchar(q);
-	}
-	_putchar('\n');
-
+char q;
+for (q = 'a'; q <= 'z'; q++)
+{
+_putchar(q);
+}
+_putchar('\n');
 }
