4ea60a55692ea5605f55705b2eb31efae7eeaef9
alzheimeer <fogniebla@hotmail.com>
2019-06-25 12:35:29 -0500

    7 and 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
-
 /**
- * int print_last digit
+ * print_last digit - prints the last digit of a number
+ * @n: number
+ * Return: q value
  */
-
-
 int print_last_digit(int n)
 {
 	int q;
+
 	q = n % 10;
-	if (q<0)
+	if (q < 0)
 	q *= -1;
 	_putchar(q + '0');
-	return(q);
+	return (q);
 }
