8eb6c071f7c1fccf89903a61ee7a8cf304b47326
alzheimeer <fogniebla@hotmail.com>
2019-06-24 20:06:07 -0500

    task 0-0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - Prints "Holberton" with a new line to the standard output.
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
