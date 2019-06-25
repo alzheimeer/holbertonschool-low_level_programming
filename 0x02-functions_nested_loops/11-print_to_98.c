588dac078dc3cdb1db2c16874596e50c1ea6bebc
alzheimeer <fogniebla@hotmail.com>
2019-06-25 13:28:45 -0500

    tasl 11 comment

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "holberton.h"
-
 /**
- * prints all numbers from n to 98
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: number initial
  * Return: 0
  */
 
