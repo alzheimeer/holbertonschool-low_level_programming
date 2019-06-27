a6fd3b63b94b1e79e87b8c9378d04c330d94eb34
alzheimeer <fogniebla@hotmail.com>
2019-06-27 23:23:51 +0000

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_square - squares.
+ * @size: size.
+ * Return: no return.
+ */
+
+
+void print_square(int size)
+{
+	int x, y;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+
+
+}
