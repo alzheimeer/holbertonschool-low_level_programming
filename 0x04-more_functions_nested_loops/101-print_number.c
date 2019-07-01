0a40988099119c62e070c2456aea93aad3ddff8a
alzheimeer <fogniebla@hotmail.com>
2019-07-01 00:11:27 -0500

    100 y 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,35 +3,67 @@
 /**
  * print_number - prints an integer.
  * @n: input
- * Return: ---.
- */
+ * Return: multiples print
+ *
+ *
+ *
+ *void print_number(int n)
+ *{
+ *
+ *	if (n < 0)
+ *	{
+ *		_putchar('-');
+ *		n = n * -1;
+ *	}
+ *	if (n < 10)
+ *	{
+ *		_putchar(n + '0');
+ *	}
+ *	if (n < 100 &&  n > 9)
+ *	{
+ *		_putchar(n / 10 + '0');
+ *		_putchar(n % 10 + '0');
+ *	}
+ *	if (n < 1000 && n > 99)
+ *	{
+ *		_putchar(n / 100 + '0');
+ *		_putchar((n / 10) % 10 + '0');
+ *		_putchar(n % 10 + '0');
+ *	}
+ *	if (n < 10000 && n > 999)
+ *	{
+ *		_putchar(n / 1000 + '0');
+ *		_putchar((n / 100) % 10 + '0');
+ *		_putchar((n / 10) % 10 + '0');
+ *		_putchar(n % 10 + '0');
+ *	}
+}*/
+
 void print_number(int n)
 {
+	unsigned int m, d, c;
+
 	if (n < 0)
 	{
-		_putchar('-');
-		n = n * -1;
+		_putchar(45);
+		m = n * -1;
 	}
-	if (n < 10)
+	else
 	{
-		_putchar(n + '0');
+		m = n;
 	}
-	if (n < 100 &&  n > 9)
+
+	d = m;
+	c = 1;
+
+	while (d > 9)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		d /= 10;
+		c *= 10;
 	}
-	if (n < 1000 && n > 99)
+
+	for (; c >= 1; c /= 10)
 	{
-		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	if (n < 10000 && n > 999)
-	{
-		_putchar(n / 1000 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
+		_putchar(((m / c) % 10) + 48);
 	}
 }
