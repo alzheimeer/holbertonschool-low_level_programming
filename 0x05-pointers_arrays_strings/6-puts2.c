4260573edf90bcaa7073b349f7ebdb7324901a61
alzheimeer <fogniebla@hotmail.com>
2019-07-02 16:45:38 -0500

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints one char out of 2
+ * @str: string
+ * Return: none
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
