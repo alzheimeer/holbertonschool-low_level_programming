a65c4f24f155ccc2310398105ca33066ee8a4ed5
alzheimeer <fogniebla@hotmail.com>
2019-07-02 23:57:33 -0500

    2adv

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - crackme
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int rand;
+	int c;
+	int x;
+
+	srand(time(NULL));
+	for (c = 0, x = 2772; x > 122; c++)
+	{
+		rand = (rand() % 125) + 1;
+		printf("%c", rand);
+		x -= rand;
+	}
+	printf("%c", x);
+
+	return (0);
+}
