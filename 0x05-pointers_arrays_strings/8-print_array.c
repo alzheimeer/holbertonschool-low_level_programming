8b6fcb6d0ec5ab4cf2cb40e7f4a7c708fff09fd2
alzheimeer <fogniebla@hotmail.com>
2019-07-02 19:33:24 -0500

    8 tash

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array
+ * @a: array.
+ * @n: number of elements
+ * Return: none
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a +i));
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+
+}
