07b83c27dc1abb102c6bcd9645aa6068ba35f0d2
alzheimeer <fogniebla@hotmail.com>
2019-07-04 11:07:18 -0500

    0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strncat - Write a function that concatenates two strings
+ * @dest: The string that receives
+ * @src: The second string that receives
+ * @n: The number of char that prints
+ *
+ * Return: 
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
