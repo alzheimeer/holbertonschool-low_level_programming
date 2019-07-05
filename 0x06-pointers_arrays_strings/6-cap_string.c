4a9d7391606c02a228e0bbebf8032517b41f7f4d
alzheimeer <fogniebla@hotmail.com>
2019-07-04 22:50:06 -0500

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,28 +7,21 @@
  */
 char *cap_string(char *s)
 {
-
-
+	int i, j;
 	char *guias = ", \t\n.;?\"(){}";
 
-	for (int i = 0; *(s + i); i++)
+	for (i = 0; *(s + i); i++)
 	{
-		for (int j = 0; guias[j]; j++)
+		for (j = 0; guias[j]; j++)
 		{
 			if (*(s + i) == guias[j])
-
-				if (*(s + (i + 1)) >= 'a' && *(s + (i + 1)) <= 'z')
-
+				if (((*(s + (i + 1))) >= 'a' && (*(s + (i + 1))) <= 'z'))
 					*(s + (i + 1)) = *(s + (i + 1)) - 32;
 		}
-
 		if (i == 0)
-
 			if (*(s + i) >= 'a' && *(s + i) <= 'z')
-
 				*(s + i) = *(s + i) - 32;
 	}
 	return (s);
-
 
 }
