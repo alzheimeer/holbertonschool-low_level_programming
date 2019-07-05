55e5284ef5512290fb00cf0b90483ecd63bc0ba6
alzheimeer <fogniebla@hotmail.com>
2019-07-04 23:50:54 -0500

    cry

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size
+ * Return: pointer dest.
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j;
+	
+	for (i= 0; n1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; n2[j] != '\0'; j++)
+	{
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return (r);
+}
