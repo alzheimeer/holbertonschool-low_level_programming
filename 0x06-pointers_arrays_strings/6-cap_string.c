32b4ecaeeaa8354590c8067c33d32bc437b3ea1b
alzheimeer <fogniebla@hotmail.com>
2019-07-04 22:36:04 -0500

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes most of the words in a string.
+ * @s: analized string.
+ *
+ * Return: String with all words capitalized.
+ */
+char *cap_string(char *s)
+{
+
+
+	char *guias = ", \t\n.;?\"(){}";
+
+	for (int i = 0; *(s + i); i++)
+	{
+		for (int j = 0; guias[j]; j++)
+		{
+			if (*(s + i) == guias[j])
+
+				if (*(s + (i + 1)) >= 'a' && *(s + (i + 1)) <= 'z')
+
+					*(s + (i + 1)) = *(s + (i + 1)) - 32;
+		}
+
+		if (i == 0)
+
+			if (*(s + i) >= 'a' && *(s + i) <= 'z')
+
+				*(s + i) = *(s + i) - 32;
+	}
+	return (s);
+
+
+}
