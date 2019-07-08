0d7362d6fe3f17e58ca177416cc28a1a81a4aea0
alzheimeer <fogniebla@hotmail.com>
2019-07-08 15:52:32 -0500

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+
+/**
+ * _strstr - Function that return the number that coincide two strings
+ * @haystack: The char to print
+ * @needle: The char to print
+ * Return: needle or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j;
+
+	while (*(needle + i))
+	{
+		for (j = 0; *(haystack + j); j++)
+		{
+			if (*(needle + i) == haystack[j])
+			{
+				return (&needle[i]);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
