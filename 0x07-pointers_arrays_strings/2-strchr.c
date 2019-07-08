4dc657ead3696ab26c3767e68b50c6a21ffa8a97
alzheimeer <fogniebla@hotmail.com>
2019-07-08 18:06:20 -0500

    2 reload

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,16 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (*(s + i++))
 	{
 		if (s[i] == c)
 			return (&s[i]);
 	}
-	return (NULL);
+	if (*(s + i) == c)
+	{
+		return (&s[i]);
+	}
+	return (0);
 }
