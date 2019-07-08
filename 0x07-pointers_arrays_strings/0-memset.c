a94c06e9d75210c85639f9e4bbd21478f67cde2f
alzheimeer <fogniebla@hotmail.com>
2019-07-08 06:46:35 -0500

    ini

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: The char to print
+ * @b: The char to print
+ * @n: unsigned int
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
