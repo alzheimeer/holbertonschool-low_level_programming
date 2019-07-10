63b1d888996d8faa0792f827ac39967958eeb154
alzheimeer <fogniebla@hotmail.com>
2019-07-10 12:30:15 -0500

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion -  prints a string in reverse
+ * @s: string
+ * Return: string return
+ **/
+
+
+
+
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
