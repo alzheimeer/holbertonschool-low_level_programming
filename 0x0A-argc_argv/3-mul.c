0d8976f8ac21e0f51fc79c6dc86bb3909d200be0
alzheimeer <fogniebla@hotmail.com>
2019-07-13 15:29:12 -0500

    [NEW]task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints multiplies two arguments
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc < 3)
+	{
+		printf("error\n");
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	return (0);
+
+}
