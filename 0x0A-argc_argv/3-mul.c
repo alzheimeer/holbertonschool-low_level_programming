84c83c1af31b226f2fb79a2ad740392e863e1cb5
alzheimeer <fogniebla@hotmail.com>
2019-07-13 15:34:10 -0500

    [NEW]task 3-1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,10 @@ int main(int argc, char *argv[])
 	if (argc < 3)
 	{
 		printf("error\n");
+		return (1);
 	}
-	else
-	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", mul);
-	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
 	return (0);
 
 }
