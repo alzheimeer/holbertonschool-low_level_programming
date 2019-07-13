19bc2392f1835a5c57d48397d8a32bb66f37348e
alzheimeer <fogniebla@hotmail.com>
2019-07-13 14:19:53 -0500

    [NEW]task 0-void

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,9 @@
  * Return: 0.
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
