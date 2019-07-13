53dc65f6feaf995bdb04f21cebd1ac2e90d2f026
alzheimeer <fogniebla@hotmail.com>
2019-07-13 14:13:17 -0500

    [NEW]task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ * Return: 0.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf ("%s\n", argv[0]);
+	return (0);
+}
