8eed658f210539340308481f7a5acd87f8b7c859
alzheimeer <fogniebla@hotmail.com>
2019-07-13 22:41:37 -0500

    [FIX] task 4-5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,23 +9,18 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	long sum = 0;
+	long sum;
 
-	for (i = 1; i < argc; i++)
+	for (i = 1, sum = 0; i < argc; i++)
 	{
-		if (*argv[i] == 48)
-		{
-		}
-		else if (!atoi(argv[i]))
+		if (*argv[i] == 0 || atoi(argv[i]) <= 0)
 		{
 			printf("Error\n");
 			return (1);
 		}
-		if (atoi(argv[i]) > 0)
-		{
 		sum += atoi(argv[i]);
-		}
 	}
+
 	printf("%ld\n", sum);
 	return (0);
 }
