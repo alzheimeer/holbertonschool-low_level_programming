f7f80ed8760a5f2329501e4e26b52555437e4363
alzheimeer <fogniebla@hotmail.com>
2019-07-16 12:43:49 -0500

    [NEW] task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *create_array - create array of char,nitializes it with a specific char.
+ * @c: char
+ * @size: integer
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *vector;
+	vector = (char*)malloc(size * sizeof(char));
+	vector[0] = 'h';
+
+	if  (vector == NULL)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		vector[i] = c;
+
+	vector[i] = '\0';
+	return (vector);
+
+}
