f1d239112d05ced66166527fd2c58359df2576f7
alzheimeer <fogniebla@hotmail.com>
2019-07-16 16:09:46 -0500

    [NEW] task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+	char *p;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+
+	p = malloc((i + j) * sizeof(char) + 1);
+	if (p == NULL)
+		return (NULL);
+
+	i = 0, j = 0;
+	while (s1[i])
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		p[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	*p = *s1;
+
+	p[i] = 0;
+	return (p);
+}
