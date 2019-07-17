79ba9290da38f1ab92f4d2a443ccd0342c782232
alzheimeer <fogniebla@hotmail.com>
2019-07-16 22:08:42 -0500

    [FIX]task 4-0

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,24 +1,20 @@
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * free_grid - Function that return a pointer
- * @grid: The width of the array
- * @height: The height of the array
- * Return: On success 1.
+ * free_grid - frees a 2 dimensional grid.
+ * @grid: multidimensional array of integers.
+ * @height: height of the grid.
+ * Return: no return
  */
 void free_grid(int **grid, int height)
 {
-	int j = 0, s = 0;
-
-	(void)height;
-	if (grid)
+	if (grid != NULL && height != 0)
 	{
-		while (*grid[s])
-			s++;
-		for (j = 0; j < s; j++)
-			free(grid[j]);
+		while (height >= 0)
+		{
+			free(grid[height]);
+			height--;
+		}
 		free(grid);
 	}
-
 }
