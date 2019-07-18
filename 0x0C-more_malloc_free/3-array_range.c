d940d83d9774f14feeb77b6ab0bc73128ef79934
alzheimeer <fogniebla@hotmail.com>
2019-07-18 16:15:45 -0500

    [FIX]task 3-0

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,7 +6,7 @@
  * *array_range - Function that allocates memory for an array of integers
  * @min: The min number
  * @max: The max number
- * Return: On success 1.
+ * Return: p
  */
 int *array_range(int min, int max)
 {
@@ -15,11 +15,11 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	n = (max - min) + 1;
+	n = (max - min + 1);
 	p = malloc(n * sizeof(int));
 	if (p == NULL)
 		return (NULL);
-	while (i <= max)
+	while (min <= max)
 	{
 		p[i] = min++;
 		i++;
