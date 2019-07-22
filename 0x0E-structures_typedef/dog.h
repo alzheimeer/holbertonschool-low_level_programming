1ae3dce7b40a0809a2a356e810a695d62227c56d
alzheimeer <fogniebla@hotmail.com>
2019-07-22 14:04:28 -0500

    [NEW]task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure task 0 dog
+ * @name: Name dog
+ * @age: Age
+ * @owner: Name owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif /* DOG_H */
