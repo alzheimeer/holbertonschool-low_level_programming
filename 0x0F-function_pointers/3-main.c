f1025d422c3c9bce5a2e3fa009ad24a22ecf3479
alzheimeer <fogniebla@hotmail.com>
2019-07-25 00:03:38 -0500

    main de calc con error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,11 +19,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (*argv[2] != '+' &&
-	    *argv[2] != '-' &&
-	    *argv[2] != '*' &&
-	    *argv[2] != '/' &&
-	    *argv[2] != '%')
+	if (argv[2][1] != 0 ||
+	    (argv[2][0] != '+' &&
+	     argv[2][0] != '-' &&
+	     argv[2][0] != '*' &&
+	     argv[2][0] != '/' &&
+	     argv[2][0] != '%'))
 	{
 		printf("Error\n");
 		exit(99);
