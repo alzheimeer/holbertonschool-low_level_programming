76938a606016638d76195f36dbf6299f108839b8
alzheimeer <fogniebla@hotmail.com>
2019-07-24 22:45:16 -0500

    [NEW]prototipos en .h estructura  y 5 funciones completas

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,14 @@
+#ifndef CALC_
+#define CALC_
+
+
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
  * struct op - Struct op
  *
@@ -9,3 +20,6 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
+
+#endif /* CALC_ */
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,57 @@
-op_add: returns the sum of a and b. Prototype: int op_add(int a, int b);
-op_sub: returns the difference of a and b. Prototype: int op_sub(int a, int b);
-op_mul: returns the product of a and b. Prototype: int op_mul(int a, int b);
-op_div: returns the result of the division of a by b. Prototype: int op_div(int a, int b);
-op_mod: returns the remainder of the division of a by b. Prototype: int op_mod(int a, int b);
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * op_add - add
+ * @a: integer
+ * @b: integer
+ * Return: result.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - check if a number is equal to 98
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - check if a number is equal to 98
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
