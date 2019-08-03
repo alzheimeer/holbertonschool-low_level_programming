c5635a0eea561962b8e1d3c308da8ea57e3abd50
alzheimeer <fogniebla@hotmail.com>
2019-08-02 23:43:56 -0500

    feature 100 advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * printBefore - free memory of all list.
+ * void: pointer to head in fucntion
+ * Return: Always success.
+ */
+
+
+void __attribute__ ((constructor)) mao()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
