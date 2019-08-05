714cda03f1f9be37e3b2284682ed5117b32516fd
alzheimeer <fogniebla@hotmail.com>
2019-08-05 18:59:31 -0500

    feature task 9 insert node in n position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: address first node
+ * @idx: index of the list where the new node should be added
+ * @n: data node
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *aux, *dlt;
+	unsigned int a = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+	}
+	new->n = n;
+	if (*head == NULL)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		aux = *head;
+		while (a < (idx - 1))
+		{
+			aux = aux->next;
+			a++;
+		}
+		dlt = aux->next;
+		aux->next = new;
+		new->next = dlt;
+	}
+	return (new);
+
+}
