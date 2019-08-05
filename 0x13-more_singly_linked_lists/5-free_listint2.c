92b8f4f0dcd3e24347fd5375f5828122c005adcc
alzheimeer <fogniebla@hotmail.com>
2019-08-05 15:40:07 -0500

    feature task 5 double pointer to free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: head of the linked list.
+ * Return: 0
+ */
+
+void free_listint2(listint_t **head)
+{
+
+	listint_t *aux = *head;
+	listint_t *next;
+
+	while (aux)
+	{
+		next = aux->next;
+		free(aux);
+		aux = next;
+	}
+	*head = NULL;
+}
