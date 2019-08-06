a11e54502be85b46a8e18a900f452af042c7fdb5
alzheimeer <fogniebla@hotmail.com>
2019-08-06 00:48:41 -0500

    fix add node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
-		printf("Error\n");
+		return (NULL);
 	}
 	new->n = n;
 	if (*head == NULL)
