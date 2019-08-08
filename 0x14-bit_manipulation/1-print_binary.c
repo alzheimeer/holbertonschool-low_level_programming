4e0f617a27f026b319aa4ec75851a1bcc307c5af
alzheimeer <fogniebla@hotmail.com>
2019-08-08 15:13:45 -0500

    fix task 1 limited zeros

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,13 +8,19 @@
 void print_binary(unsigned long int n)
 {
 	int sup = 4 * sizeof(int);
+	int j = 0;
 
 	while (sup >= 0)
 	{
 		if (n & 1 << sup)
+		{
+			j = 1;
 			_putchar(49);
-		else
+		}
+		else if (j == 1)
 			_putchar(48);
 		sup--;
 	}
+	if (j == 0)
+		_putchar(48);
 }
