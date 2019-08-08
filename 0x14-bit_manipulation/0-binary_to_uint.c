3cf2697d010d774df165ec95f248e892efff77d9
alzheimeer <fogniebla@hotmail.com>
2019-08-08 13:51:51 -0500

    feature task 0 convert binary to decimal and files base

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: binary
+ * Return: the converted number
+ * or 0 if there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	int n = 0;
+	int z = 1;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i])
+	{
+		if ((b[i] != '0') & (b[i] != '1'))
+			return (0);
+
+		i++;
+	}
+	for (; i > -1; i--)
+	{
+		if (b[i - 1] == 49)
+		{
+			n = n + z;
+			z = z * 2;
+		}
+		else
+			z = z * 2;
+	}
+	return (n);
+
+}
