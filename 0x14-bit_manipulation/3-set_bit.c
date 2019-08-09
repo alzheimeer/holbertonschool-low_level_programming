668ba4cc01691552891c427b717255ecd8df5061
alzheimeer <fogniebla@hotmail.com>
2019-08-08 19:09:31 -0500

    fix task 3 short

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,44 +8,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	char b[64];
-	int i = 0, sup = 62, j = 0, nm = 0, z = 1;
-	long int a = 1;
+	unsigned long int s;
 
-	for (; i < 63; i++)
-	{
-		b[i] = 0;
-	}
-	i = 0;
+	s = 0;
 	if (index > 63)
 		return (-1);
-	while (sup >= 0)
-	{
-		if (*n & a << sup)
-		{
-			j = 1;
-			b[i] = 1;
-		}
-		else if (j == 1)
-			b[i] = 0;
-		sup--;
-		i++;
-	}
-	if (j == 0)
-		b[i] = 0;
-
-	b[62 - index] = 1;
-
-
-	for (i = 62; i > 0; i--)
-	{
-		if (b[i] == 1)
-		{
-			nm = nm + z;
-			z = z * 2;
-		}
-		else
-			z = z * 2;
-	}
-	return (*n = nm);
+	s = *n | (1 << index);
+	*n = s;
+	return (1);
 }
