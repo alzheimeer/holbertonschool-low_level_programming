48930de075ed8314877da00c0f1069748815ac40
alzheimeer <fogniebla@hotmail.com>
2019-08-13 16:20:40 -0500

    fix task 1 checks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,8 +12,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
-
+	if (text_content == NULL)
+		text_content = "";
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 	write(fd, text_content, strlen(text_content));
