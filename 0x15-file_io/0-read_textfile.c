33013efb0a3b217aac4c7e6371498e20a0aadbc1
alzheimeer <fogniebla@hotmail.com>
2019-08-13 15:18:15 -0500

    fix task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,21 +9,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 
-	int fd, length;
-	char buf[letters];
+	int fd, length2, lenght1;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
+	buf = malloc(letters * sizeof(char));
+
 	if (buf == NULL)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
-	length = write(STDOUT_FILENO, buf, read(fd, buf, letters));
-	if (length == -1)
+
+	lenght1 = read(fd, buf, letters);
+	length2 = write(STDOUT_FILENO, buf, length1);
+
+	if (length2 == -1)
 		return (0);
 	close(fd);
-	return (length);
-
-
+	free(buf);
+	return (length2);
 }
