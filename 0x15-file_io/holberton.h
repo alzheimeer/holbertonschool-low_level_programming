13606bd21e710c48715cf043b8ea8780e670298e
alzheimeer <fogniebla@hotmail.com>
2019-08-13 14:27:17 -0500

    feature task 0 and files base

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,15 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stddef.h>
-#include <unistd.h>
-#include <stdio.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* HOLBERTON_H */
+
+
+
+#endif
