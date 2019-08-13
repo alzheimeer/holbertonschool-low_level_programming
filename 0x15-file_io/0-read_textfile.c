b9fdf94b2914df9cfb677d024c196f0e2ba6ba5c
alzheimeer <fogniebla@hotmail.com>
2019-08-13 14:56:26 -0500

    feature task 0 and files base

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <fcntl.h>
 #include "holberton.h"
-#include <unistd.h>
 
 /**
  * read_textfile - returns the actual number of letters
