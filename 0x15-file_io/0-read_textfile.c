9ecb58207f311ad4af63af7da3285f0201b4a677
alzheimeer <fogniebla@hotmail.com>
2019-08-13 14:33:58 -0500

    feature task 0 and files base

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include <string.h>
 #include "holberton.h"
 
 /**
