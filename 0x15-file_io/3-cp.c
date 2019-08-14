8baa73f25e6c13bfeccb3ae2fc28c7841de624f6
alzheimeer <fogniebla@hotmail.com>
2019-08-13 22:28:35 -0500

    fix task 3-5

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,8 +7,8 @@
  */
 int main(int argc, char *argv[])
 {
+	int fd, fd2, l2, l1 = 1024;
 	char buf[1024];
-	int fd, fd2, l1 = 1024, c0, c1;
 
 	if (argc != 3)
 	{
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
-	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -31,19 +31,18 @@ int main(int argc, char *argv[])
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98); }
-		l1 = write(fd2, buf, l1);
-		if (l1 == -1)
+		l2 = write(fd2, buf, l1);
+		if (l2 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99); } }
-	c0 = close(fd), c1 = close(fd2);
-	if (c0 == -1)
-		{
+			exit(99); }}
+	if (close(fd) == -1)
+	{
 		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd);
 		exit(100); }
-	c1 = close(fd2);
-	if (c1 == -1)
-		{
+	if (close(fd2) == -1)
+	{
 		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd2);
 		exit(100); }
-	return (0); }
+	return (0);
+}
