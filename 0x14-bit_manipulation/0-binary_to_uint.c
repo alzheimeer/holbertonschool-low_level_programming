a778516a5d1fd4175a474222bbced72090a32e4b
alzheimeer <fogniebla@hotmail.com>
2019-08-24 09:34:29 -0500

    fix rare 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "holberton.h"
-
 /**
  * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: binary
@@ -7,19 +7,19 @@
  * or 0 if there is one or more chars in the string b that is not 0 or 1
  * b is NULL
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
-	int n = 0;
-	int z = 1;
+	int i;
+	unsigned int n, z;
 
+	i = 0;
+	n = 0;
+	z = 1;
 	if (b == NULL)
 		return (0);
-
 	while (b[i])
 	{
-		if ((b[i] != '0') & (b[i] != '1'))
+		if ((b[i] != '0') && (b[i] != '1'))
 			return (0);
 
 		i++;
