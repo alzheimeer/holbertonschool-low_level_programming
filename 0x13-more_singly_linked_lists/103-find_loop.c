3ffb68fb7b4de2088575a9f355566a7232613938
alzheimeer <fogniebla@hotmail.com>
2019-09-09 23:29:22 -0500

    feature find loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - find the loop in a linked list
+ * @head: first element of the list
+ * Return: the node where estarts the loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *lento = head, *rap = head;
+
+	if (list == NULL)
+		return (0);
+	while (lento && rap && rap->next)
+	{
+		lento = lento->next;
+		rap = rap->next->next;
+
+		if (lento == rap)
+		{
+			return (head);
+		}
+	}
+	return (NULL);
+}
