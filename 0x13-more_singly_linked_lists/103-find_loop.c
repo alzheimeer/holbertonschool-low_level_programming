9ec4eb559c465e21f0eb0be47aac1e30f88329f3
alzheimeer <fogniebla@hotmail.com>
2019-09-09 23:37:23 -0500

    feature find loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,7 +11,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *lento = head, *rap = head;
 
-	if (list == NULL)
+	if (head == NULL)
 		return (0);
 	while (lento && rap && rap->next)
 	{
