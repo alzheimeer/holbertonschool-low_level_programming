d2be299cd505be6f04e6977ba0a8379bb6df20ec
alzheimeer <fogniebla@hotmail.com>
2019-09-14 15:15:47 -0500

    feature 7 task insert

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - adds node at the beginning of a dlistint_t list.
+ * @h: pointer to the first element of the list
+ * @idx:index
+ * @n:number
+ * Return: the pointer to a new node at the beginning of a dlistint_t list
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *aux, *dlt;
+	unsigned int a = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*h == NULL)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+	else
+	{
+		aux = *h;
+		while (a < (idx - 1))
+		{
+			aux = aux->next;
+			a++;
+		}
+		if (aux->next != NULL)
+		{
+			dlt = aux->next;
+			new->prev = aux;
+			aux->next = new;
+			new->next = dlt;
+			dlt->prev = new;
+		}
+		else
+		{
+			new = add_dnodeint_end(h, n);
+			return (new);
+		}
+	}
+	return (new);
+}
