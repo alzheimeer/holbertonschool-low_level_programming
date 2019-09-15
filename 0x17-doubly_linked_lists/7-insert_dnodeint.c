9073c7ef63cc83831f027d873f6a36276da675ff
alzheimeer <fogniebla@hotmail.com>
2019-09-15 14:56:30 -0500

    fix 7 task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *aux, *dlt;
 	unsigned int a = 0;
 
+	if (h == NULL)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -24,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	else
 	{
-		while (a < (idx - 1))
+		while (a < (idx))
 		{
 			aux = aux->next;
 			a++;
