2a183319e86efa7a966a92c6da1e8ea1f61c2ed1
alzheimeer <fogniebla@hotmail.com>
2019-09-15 16:01:32 -0500

    fix 7 task insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,41 +12,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *aux, *dlt;
 	unsigned int a = 0;
 
-	if (h == NULL)
+	aux = *h;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	while (a < (idx - 1))
+	{
+		aux = aux->next;
+		a++;
+	}
+	if (aux->next == NULL)
+		return (add_dnodeint_end(h, n));
+	if (idx >= a + 2)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	aux = *h;
-	if (idx == 0 || *h == NULL)
-	{
-		new = add_dnodeint(h, n);
-		return (new);
-	}
-	else
-	{
-		while (a < (idx - 1))
-		{
-			aux = aux->next;
-			a++;
-		}
-		if (idx >= a + 2)
-			return (NULL);
-		if (aux->next != NULL)
-		{
-			dlt = aux->next;
-			aux->next = new;
-			new->prev = aux;
-			new->next = dlt;
-			dlt->prev = new;
-			return (new);
-		}
-		else
-		{
-			new = add_dnodeint_end(h, n);
-			return (new);
-		}
-	}
+	dlt = aux->next;
+	new->next = dlt;
+	new->prev = aux;
+	aux->next = new;
+	dlt->prev = new;
 	return (new);
 }
