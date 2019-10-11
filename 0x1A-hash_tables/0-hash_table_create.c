ab0c505142fee7d1957cb6efd53cc20d46ced0c9
alzheimeer <fogniebla@hotmail.com>
2019-10-11 07:06:32 -0700

    feature task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - initialize a hash table
+ * @size: size of the array of the table
+ * Return: the hash table initialize
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = NULL;
+	unsigned long int i;
+
+	if (size < 1) return (NULL);
+	/* malloc table*/
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	/*malloc pointers to the head nodes*/
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+		return (NULL);
+	/*Initialize elements pointing*/
+	for( i = 0; i < size; i++ )
+	{
+		hash_table->array[i] = NULL;
+	}
+	hash_table->size = size;
+	return (hash_table);
+}
