5eac8474344da7070ff6c8e371476acad00f45b8
alzheimeer <fogniebla@hotmail.com>
2019-10-11 13:57:49 -0700

    feature task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - gets an element
+ * @ht: pointer to hash table
+ * @key: key
+ * Return: string value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *aux;
+	unsigned long int index = 0;
+
+	if (!ht || !key || !*key)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	aux = ht->array[index];
+	while (aux)
+	{
+		if (strcmp(key, aux->key) == 0)
+			return(aux->value);
+		aux = aux->next;
+	}
+	return(0);
+}
