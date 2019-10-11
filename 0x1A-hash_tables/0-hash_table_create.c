7a4f49be34bdf7767a11f7cce6696f33d075b008
alzheimeer <fogniebla@hotmail.com>
2019-10-11 16:26:31 -0700

    fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,6 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
+	hash_table->size = size;
 	/*malloc pointers to the head nodes*/
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
@@ -24,9 +25,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	/*Initialize elements pointing*/
 	for (i = 0; i < size; i++)
-	{
 		hash_table->array[i] = NULL;
-	}
-	hash_table->size = size;
 	return (hash_table);
 }
