d27e9b624eb1927284c9b301407c99a98e40d43b
alzheimeer <fogniebla@hotmail.com>
2019-10-11 10:13:19 -0700

    fix task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,27 +11,38 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
-	hash_node_t *next;
-	unsigned long int index, size;
+	hash_node_t *head;
+	unsigned long int i, size;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (!ht || !key || !*key || !value)
 		return (0);
-
-	size = ht->size;
-	index = key_index((const unsigned char *)key, size);
-	next = ht->array[index];
-	if (next != NULL && strcmp(next->key, key) == 0)
-	{
-		free(next->value);
-		next->value = strdup(value);
-		return (1);
-	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = next;
-	next = new_node;
-	return (1);
+
+	size = ht->size;
+	i = key_index((const unsigned char *)key, size);
+	head = ht->array[i];
+	if (head == NULL)
+	{
+		new_node->next = head;
+		head = new_node;
+		return (1);
+	}
+	else
+	{
+		while (head)
+		{
+			if (!strcmp(key, head->key))
+			{
+				free(head->value);
+				head->value = strdup(value);
+				return (1);
+			}
+			head = head->next;
+		}
+	}
+	return (0);
 }
