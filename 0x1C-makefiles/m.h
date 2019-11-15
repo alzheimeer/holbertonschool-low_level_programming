33090f515bb2c99e47d468c071a3bab912e057db
alzheimeer <fogniebla@hotmail.com>
2019-11-15 13:46:19 -0500

    feature task 0 makefile

---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,9 @@
+#ifndef __pepe_H__
+#define __pepe_H__
+#include <stdlib.h>
+#include <stdio.h>
+void printx(void);
+
+
+
+#endif
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include "m.h"
+#include <stdio.h>
+
+/**
+ * main - print.
+ * Return: 0.
+ */
+void main(void)
+{
+	 printx();
+
+}
