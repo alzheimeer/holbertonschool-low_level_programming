d548e8d86f205c3696c93017321910d591222b53
alzheimeer <fogniebla@hotmail.com>
2019-12-04 17:53:44 -0500

    feature task 13 nodes tree

---
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_nodes - counts nodes with at least one child in a binary tree
+ *@tree: pointer to the root node of the tree to count the number of nodes
+ *Return: tree is NULL, the function must return 0
+ **/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int a, b;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	if (tree->parent != NULL)
+	{
+		if (tree->left || tree->right)
+			return (1);
+	}
+
+	a = binary_tree_nodes(tree->left);
+	b = binary_tree_nodes(tree->right);
+	return (a + b + 1);
+}
